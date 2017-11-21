5c2470dd0c05491823260cd6ae46e0481b548bb3
thirdink <rumitwashere@gmail.com>
2017-11-21 15:53:09 +0545

    till client side cache

--- news/src/App.js
+++ news/src/App.js
@@ -6,10 +6,16 @@ import list from '../src/list';
 const DEFAULT_QUERY = 'react';
 const PATH_BASE = 'http://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
+const DEFAULT_PAGE=0;
+const DEFAULT_HPP=100;
+
+
 const PARAM_SEARCH = 'query=';
+const PARAM_PAGE='page=';
+const PARAM_HPP='hitsPerPage=';
 
 // const url = PATH_BASE + PATH_SEARCH + '?' + PARAM_SEARCH + DEFAULT_QUERY;
-const url = `${PATH_BASE}${PATH_SEARCH}${PARAM_SEARCH}?${DEFAULT_QUERY}`;
+const url = `${PATH_BASE}${PATH_SEARCH}${PARAM_SEARCH}?${DEFAULT_QUERY}&${PARAM_PAGE}&${PARAM_HPP}${DEFAULT_HPP}`;
 console.log(url);
 
 
@@ -24,25 +30,63 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      list
+      results:null,
+      searchKey:'',
+      searchTerm: DEFAULT_QUERY
     }
     this.removeItem=this.removeItem.bind(this);// alwasys bind inside the class constructor
     this.searchValue=this.searchValue.bind(this);
+    this.fetchTopStories = this.fetchTopStories.bind(this);
+    this.setTopStories = this.setTopStories.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+  // set top stories
+  setTopStories(result){
+    // get the hits nad page from the result
+    const {hits,page} = result;
+    // // meaning page is not 0, button has been clicked, page might be 1 or 2
+    // old hits are already available in the state
+    const {searchKey,results} = this.state;
+    const oldHits = results && results[searchKey]?results[searchKey].hits:[];
+    const updatedHits = [...oldHits, ...hits];
+    
+    this.setState({ results: { ...results, [searchKey]: {hits: updatedHits, page} }});
+  }
+  // fetch top stories
+  fetchTopStories(searchTerm, page){
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
+    .then(response => response.json()).then(result => this.setTopStories(result))
+    .catch(e => e);
+  }
+  // component did mount 
+  componentDidMount(){
+    const {searchTerm} = this.state;
+    this.setState({ searchKey: searchTerm});
+    this.fetchTopStories(this.state.searchTerm,DEFAULT_PAGE);
+  }
+  // on th search submit function
+  onSubmit(event){
+    const {searchTerm} = this.state;
+    this.setState({ searchKey: searchTerm});
+    this.fetchTopStories(searchTerm, DEFAULT_PAGE);
+    event.preventDefault();
   }
   removeItem(id){
-    console.log('Remove Item');
+    const {result } = this.state;
+    //console.log('Remove Item');
     // using javascript filter method 
     // we can filter out the clicked item and render the updated list
     function isNotId(item){
       return item.objectID!== id;
     }
     // create a new updated list
-    const updatedList = this.state.list.filter(isNotId);
+    const updatedList = result.hits.filter(isNotId);
     // assign the new updated list to the list using setState method
-    this.setState({
-      list: updatedList,
-      searchTerm:''
-    });
+    // this.setState({
+    //   result: Object.assign({},this.state.result,{hits: updatedList})
+      
+    // });
+    this.setState({ result:{...result, hits: updatedList}});
   }
   searchValue(event){
     // console.log(event);
@@ -50,9 +94,15 @@ class App extends Component {
 
   }
   render() {
-    const {list, searchTerm} = this.state;
-  console.log(this);
+    const {results, searchTerm,searchKey} = this.state;
+    const page = (results && results[searchKey] && results[searchKey].page) ||0;
+    const list = (results && results[searchKey]&&results[searchKey].hits)||[];
+    // if(!result){
+    //   return null;
+    // }
+    console.log(this);
     return (
+    
       <div>
       <Grid fluid>
         <Row>
@@ -60,16 +110,25 @@ class App extends Component {
           <Search 
           onChange={this.searchValue} 
             value={ searchTerm }
+            onSubmit= {this.onSubmit}
             >News App</Search>
           </div>
         </Row>
       </Grid>
 
+      
       <Table 
       list={list}
       searchTerm={searchTerm}
       removeItem={this.removeItem}
-      />
+      /> 
+      
+      <div className="text-center alert">
+        <Button className="btn btn-success" 
+        onClick={()=> this.fetchTopStories(searchTerm,page + 1) }>
+          Load More
+        </Button>
+      </div>
 
       </div>
     );
@@ -82,9 +141,9 @@ class App extends Component {
     
 //   }
 //}
-const Search = ({ onChange, value, children})=>{
+const Search = ({ onChange, value, children, onSubmit})=>{
   return(
-    <form>
+    <form onSubmit={onSubmit}>
       <FormGroup>
       <h1 style={{fontweight:'bold'}}>  {children}  </h1> 
       <hr style={{border:'1px solid black',width:'100px'}}/>
@@ -95,7 +154,7 @@ const Search = ({ onChange, value, children})=>{
         onChange={onChange} 
         value={ value }/>
         <span className="input-group-btn">
-        <button className="btn btn-primary searchBtn" type="submit">Search</button>
+        <Button className="btn btn-primary searchBtn" type="submit">Search</Button>
         </span>
 
       </div>
@@ -112,7 +171,8 @@ const Table =({list,searchTerm,removeItem})=>{
   return (
     <div className="col-ms-10 col-sm-offset-1">
         {
-          list.filter(isSearched(searchTerm)).map(item=>{
+          // list.filter(isSearched(searchTerm)).map(item=>{
+            list.map(item=>{
             return (
               <div key={item.objectID}>
               <h1><a href={item.url}>{item.title}</a> by {item.author}</h1>
@@ -142,14 +202,11 @@ const Table =({list,searchTerm,removeItem})=>{
 //     )
 //   }
 // }
-function Button({onClick,children,className}){
-  return(
-    <button 
-    className={ className }
-    onClick={onClick}>
-    {children}
-    </button>
-  )
-}
+const Button = ({ onClick, children, className='' }) => 
+<button
+  className={ className }  
+  onClick={ onClick } >
+  { children }
+</button>
 
 export default App;
